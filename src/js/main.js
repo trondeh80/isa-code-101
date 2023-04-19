1cadee1d668e6110d661ca4df23363a3817db50a
Trond Erling Hundal <trond.erling@gmail.com>
2023-04-19 16:22:15 +0200

    Add some code for step 2

---
+++ src/js/main.js
@@ -0,0 +1,8 @@
+const baconURL = "https://baconipsum.com/api/?type=meat-and-filler";
+
+start();
+
+async function start() {
+    // Todo: Fetch bacon info and print on webpage!
+    const baconDomNode = document.getElementById("bacon");
+}
